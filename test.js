85bd61a1988497588799a203b91d2c958505c510
Raghad Sami <80597441+RaghadSamiAli@users.noreply.github.com>
2021-03-16 12:17:49 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-//const Name ="Raghad"
-//console.log(Name);
- 
-=======
 const Name ="Raghad"
 console.log(Name);
 
->>>>>>> 4e3dd5792eb5f455a1b849bab9ce4e5a977bf611
 const greet = (Name)=> {
     console.log('Hello' , Name);
 }
 
 greet("Ahmad");
-<<<<<<< HEAD
-greet("Raghad");
-=======
-greet("Raghad");
->>>>>>> 4e3dd5792eb5f455a1b849bab9ce4e5a977bf611
+
