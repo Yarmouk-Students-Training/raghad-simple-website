3ab4dcaa45108a5f241365c6501c587dcc77207e
Raghad <2017901047@ses.yu.edu.jo>
2021-03-18 20:05:09 +0200

    Raghad

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { create, method, result } = require('lodash');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // express app 
 const app = express();
@@ -26,16 +27,17 @@ app.set('view engine', 'ejs');
 // })
 // middleare & static files 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 
 //mongoose and mongo sandbox routes
- app.get('/add-blog',(req, res)=>{
+ //app.get('/add-blog',(req, res)=>{
 //     const blog = new Blog({
 //         title:'new blog',
 //         snippet: 'about my new blog 2',
 //         body:'more about my new blog'
-     });
+//     });
 
 //     blog.save().then((result)=>{
 //         res.send(result)
@@ -83,10 +85,36 @@ app.get('/about', (req,res)=>{
 });
 // blog routes
 app.get('/blogs', (req,res)=>{
-    Blog.find().sort({createdAt:1}).then((result)=>{
+    Blog.find().sort({createdAt:-1}).then((result)=>{
         res.render('indexx',{title:'All blogs',blogs:result})
     }).catch((err)=>{console.log(err);})
 });
+
+app.post('/blogs', (req,res)=>{
+    const blog =new Blog(req.body);
+
+    blog.save().then((result)=>{
+        res.redirect('/blogs');
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req,res)=>{
+    const id = req.params.id;
+    Blog.findById(id).then(result => {
+        res.render('details',{blog:result ,title:'Blog Datails' , })
+    }).catch(err => {console.log(err);})
+})
+
+app.delete('/blogs/:id', (req , res)=>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id).then(result =>{
+        res.json({redirect:'/blogs'});        
+    }).catch(err => {
+        console.log(err);
+    })
+})
 
 app.get('/blogs/create' , (req ,res)=>{
     res.render('create',{title: 'Create'});
